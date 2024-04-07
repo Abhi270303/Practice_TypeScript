83f9da849258f0ec6b97c5bd3f2a168c9e9f24a5
Abhishek yadav <barrelabhi45@gmail.com>
2024-04-07 20:30:49 +0530

    Intro to TypeScript

---
+++ 01Basics/intro.js
@@ -0,0 +1,3 @@
+var user = { name: "Abhishek", age: 10 };
+console.log("Abhishek");
+console.log("user.email"); //email does not exist, it gives error, butwont stop from compiling the code
---
+++ 01Basics/intro.ts
@@ -0,0 +1,4 @@
+const user = {name: "Abhishek" , age: 10}
+
+console.log("Abhishek")
+console.log("user.email") //email does not exist, it gives error, butwont stop from compiling the code
